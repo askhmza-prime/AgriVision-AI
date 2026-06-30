e9e63f593732dc80d708c88fd50420cee0a9e994
askhmza-prime <askhmza@gmail.com>
2026-06-30 21:39:29 +0530

    Add Navbar component with navigation links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,35 @@
+"use client";
 
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full flex justify-between items-center px-8 py-5 bg-white shadow-sm sticky top-0 z-50">
+
+      <Link
+      href="/"
+      className="text-3xl font-bold text-green-700">
+
+      🌿 AgriVision AI
+
+      </Link>
+
+      <div className="flex gap-8 font-medium">
+
+        <Link href="/">
+          Home
+        </Link>
+
+        <Link href="/detect">
+          Detection
+        </Link>
+
+        <Link href="/about">
+          About
+        </Link>
+
+      </div>
+
+    </nav>
+  );
+}
