7fef79639ed3a717c8db366da829461a2045dac6
askhmza-prime <askhmza@gmail.com>
2026-06-30 21:56:28 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#22c55e",
+        secondary: "#15803d",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
