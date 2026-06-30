5bc578a03a6beecc80d95d36c41cd537a75be8e8
askhmza-prime <askhmza@gmail.com>
2026-06-30 21:42:57 +0530

    Add Features, CTA, and Footer components to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
 
-export default function Home(){
-
-return(
-
-<>
-
-<Navbar/>
-
-<Hero/>
-
-</>
-
-)
-
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <CTA />
+      <Footer />
+    </>
+  );
 }
