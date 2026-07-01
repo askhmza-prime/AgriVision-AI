195a8d5dd9856ec0a91acc80b38a97fa40e22a21
askhmza-prime <askhmza@gmail.com>
2026-07-01 09:53:43 +0530

    Create api.ts

---
+++ lib/api.ts
@@ -0,0 +1,17 @@
+const API_URL = "https://agrivision-ai-ktvr.onrender.com/predict";
+
+export async function predictDisease(file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("Prediction failed");
+  }
+
+  return response.json();
+}
