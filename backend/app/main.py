2e692174889d0eebd63e5fc1dd8ceb24771f7d4a
askhmza-prime <askhmza@gmail.com>
2026-07-08 19:52:35 +0530

    Implement EXIF data removal in image processing
    Added a function to remove EXIF data from images before processing. Updated the predict_leaf route to utilize this new function.

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,13 +3,22 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import numpy as np
 import cv2
+import os
+
+from PIL import Image
+import io
 
 from .predictor import predict
 
-app = FastAPI(title="AgriVision AI")
 
-from fastapi.middleware.cors import CORSMiddleware
-import os
+app = FastAPI(
+    title="AgriVision AI"
+)
+
+
+# --------------------
+# CORS SECURITY
+# --------------------
 
 ALLOWED_ORIGINS = [
     os.getenv(
@@ -17,6 +26,7 @@ ALLOWED_ORIGINS = [
         "https://your-agrivision-domain.vercel.app"
     )
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,40 +37,105 @@ app.add_middleware(
 )
 
 
+# --------------------
+# REMOVE EXIF DATA
+# --------------------
+
+def remove_exif(contents):
+
+    image = Image.open(
+        io.BytesIO(contents)
+    ).convert("RGB")
+
+
+    output = io.BytesIO()
+
+
+    image.save(
+        output,
+        format="JPEG"
+    )
+
+
+    return output.getvalue()
+
+
+# --------------------
+# ROUTES
+# --------------------
+
 @app.get("/")
 def home():
+
     return {
-        "message": "AgriVision AI Backend Running 🚀"
+        "message":
+        "AgriVision AI Backend Running 🚀"
     }
 
 
+
 @app.post("/predict")
-async def predict_leaf(file: UploadFile = File(...)):
+async def predict_leaf(
+    file: UploadFile = File(...)
+):
+
     try:
+
         contents = await file.read()
+
         await file.close()
 
-        image = np.frombuffer(contents, np.uint8)
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        # remove image metadata
+
+        clean_contents = remove_exif(
+            contents
+        )
+
+
+        image = np.frombuffer(
+            clean_contents,
+            np.uint8
+        )
+
+
+        image = cv2.imdecode(
+            image,
+            cv2.IMREAD_COLOR
+        )
+
 
         if image is None:
+
             raise HTTPException(
                 status_code=400,
                 detail="Invalid image file."
             )
 
-        result = predict(image)
+
+        result = predict(
+            image
+        )
+
 
         del image
         del contents
+        del clean_contents
+
 
         return result
 
+
+
     except HTTPException:
+
         raise
 
+
+
     except Exception:
+
         raise HTTPException(
             status_code=500,
             detail="Prediction failed."
-            )
+        )
